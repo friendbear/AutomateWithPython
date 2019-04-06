410aa1d60c34be6b592558731e7fbd51e343a9c4
friendbear <friendbear@users.noreply.github.com>
2019-04-06 15:35:30 +0900

    range method

--- basic/fiveTimes.py
+++ basic/fiveTimes.py
@@ -3,3 +3,8 @@ print('Your name?')
 for i in range(5):
     print('Goemon (' + str(i) + ')')
 
+for i in range(1, 5):
+    print('Goemon (' + str(i) + ')')
+
+for i in range(5, 0, -1):
+    print('Goemon (' + str(i) + ')')
