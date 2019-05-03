2e1f7f2192c59149727ea1bc84095658a78260bb
friendbear <friendbear@users.noreply.github.com>
2019-05-03 17:02:00 +0900

    re module

--- automation/isPhoneNumber.py
+++ automation/isPhoneNumber.py
@@ -1,4 +1,6 @@
 
+import re
+
 def is_phone_number(text):
     if len(text) != 12:
         return False
@@ -17,6 +19,12 @@ def is_phone_number(text):
             return False
     return True
 
+def is_phone_number_with_regex(text):
+    phone_num_regex = re.compile(r'([0-9]{3}-[0-9]{3}-[0-9]{4})')
+    no = phone_num_regex.search(text) # => Match Object
+
+    return no != None
+
 def find_phone_number(text):
     for i in range(len(text)):
         chunk = text[i:i+12]
@@ -24,10 +32,26 @@ def find_phone_number(text):
             return True
     return False
 
+def findall_phone_number_with_regex(text):
+    phone_num_regex = re.compile(r'([\d]{3})-([\d]{3})-([\d]{4})')
+
+    return phone_num_regex.findall(text)
+
+
+# Test Code
 print(is_phone_number('415-555-4242'))
 print(is_phone_number('abcccccccccc'))
 
-message = 'abcke415-444-2222kkeafe'
+print(is_phone_number_with_regex('415-555-4242'))
+print(is_phone_number_with_regex('abcccccccccc'))
+
+message = 'abcke415-444-2222kkeafe032-039-9283--'
 print(find_phone_number(message))
+
+# list and tuple
+nums: (str, str, str) = findall_phone_number_with_regex(message)
+for n in nums:
+    for i in list(n):
+        print(i)
 
 
