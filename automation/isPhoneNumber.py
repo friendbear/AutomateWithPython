c90763b2fbca5a701cb25c7b0e7eee4d74272f6c
friendbear <friendbear@users.noreply.github.com>
2019-05-03 15:09:19 +0900

    Add Function

--- automation/isPhoneNumber.py
+++ automation/isPhoneNumber.py
@@ -17,7 +17,17 @@ def is_phone_number(text):
             return False
     return True
 
+def find_phone_number(text):
+    for i in range(len(text)):
+        chunk = text[i:i+12]
+        if is_phone_number(chunk):
+            return True
+    return False
+
 print(is_phone_number('415-555-4242'))
 print(is_phone_number('abcccccccccc'))
+
+message = 'abcke415-444-2222kkeafe'
+print(find_phone_number(message))
 
 
